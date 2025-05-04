ceab62be0c524db4e527662d86f3a5d09af5a9a8
javadbat <javadbat@gmail.com>
2025-05-04 11:52:59 +0330

    update demo path

--- stories/JBPasswordInput.stories.tsx
+++ stories/JBPasswordInput.stories.tsx
@@ -2,7 +2,7 @@ import { JBPasswordInput, Props } from 'jb-password-input/react';
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<Props> = {
-  title: "Example/form elements/Inputs/JBPasswordInput",
+  title: "Components/form elements/Inputs/JBPasswordInput",
   component: JBPasswordInput,
 };
 export default meta;
