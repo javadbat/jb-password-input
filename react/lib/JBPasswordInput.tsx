18d32c4f3dd415f736181b607d5c01df61758496
javadbat <javadbat@gmail.com>
2026-01-25 00:19:05 +0330

    feat: add size variants

--- react/lib/JBPasswordInput.tsx
+++ react/lib/JBPasswordInput.tsx
@@ -1,16 +1,16 @@
 'use client';
 import React, { useRef, useEffect, useState, useImperativeHandle } from 'react';
-import {BaseProps, useJBInputAttribute } from 'jb-input/react';
+import {BaseProps, useJBInputAttribute, type DirectProps } from 'jb-input/react';
 import 'jb-password-input';
 
 // eslint-disable-next-line no-duplicate-imports
 import {useJBInputEvents } from 'jb-input/react';
 // eslint-disable-next-line no-duplicate-imports
 import {type JBPasswordInputWebComponent} from 'jb-password-input';
+import type { SizeVariants } from 'jb-input';
 
-interface JBPasswordInputType extends React.DetailedHTMLProps<React.HTMLAttributes<JBPasswordInputWebComponent>, JBPasswordInputWebComponent> {
-  "class"?: string,
-}
+type JBPasswordInputType = React.DetailedHTMLProps<React.HTMLAttributes<JBPasswordInputWebComponent>, JBPasswordInputWebComponent> & DirectProps;
+
 declare module "react" {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace JSX {
@@ -21,23 +21,23 @@ declare module "react" {
 }
 // eslint-disable-next-line react/display-name
 const JBPasswordInput = React.forwardRef<JBPasswordInputWebComponent | undefined,Props>((props, ref) => {
+
   const element = useRef<JBPasswordInputWebComponent>(null);
-  const [refChangeCount, refChangeCountSetter] = useState(0);
   useImperativeHandle(
     ref,
     () => (element ? element.current : undefined),
     [element],
   );
-  useEffect(() => {
-    refChangeCountSetter(refChangeCount + 1);
-  }, [element.current]);
-  useJBInputAttribute(element,props);
-  useJBInputEvents<JBPasswordInputWebComponent>(element,props);
+   const { onBeforeinput, onBlur, onChange, onEnter, onFocus, onInput, onKeydown, onKeyup, size, disabled, error, inputmode, label, message, name, placeholder, required, type, validationList, value, ...standardProps } = props;
+  // props that directly set in jsx dom and need no process or property set
+  const directProps: Required<DirectProps> = { label, message, name, placeholder, size, type, error,inputmode }
+  useJBInputEvents(element, { onBeforeinput, onBlur, onChange, onEnter, onFocus, onInput, onKeydown, onKeyup });
+  useJBInputAttribute(element, { disabled, required, validationList, value });
   useEffect(() => {
       element.current.minLength = props.minLength;
   }, [props.minLength]);
   return (
-    <jb-password-input ref={element} class={props.className?props.className:''}>
+    <jb-password-input ref={element} {...directProps} {...standardProps} >
       {props.children}
     </jb-password-input>
   );
--- stories/JBPasswordInput.stories.tsx
+++ stories/JBPasswordInput.stories.tsx
@@ -1,6 +1,6 @@
+import React from 'react';
 import { JBPasswordInput, Props } from 'jb-password-input/react';
 import type { Meta, StoryObj } from '@storybook/react';
-
 const meta: Meta<Props> = {
   title: "Components/form elements/Inputs/JBPasswordInput",
   component: JBPasswordInput,
@@ -20,5 +20,20 @@ export const MinimumLength: Story = {
     label: 'with minimum length',
     message: "enter value with less than 8 char to see error",
     minLength: 8
+  }
+};
+
+
+export const Sizes: Story = {
+  render:()=>{
+    return(
+      <div style={{display:'flex', flexDirection:'column' ,gap:'0.5rem'}}>
+        <JBPasswordInput label='xl' size='xl'/>
+        <JBPasswordInput label='lg' size='lg'/>
+        <JBPasswordInput label='md' size='md'/>
+        <JBPasswordInput label='sm' size='sm'/>
+        <JBPasswordInput label='xs' size='xs'/>
+      </div>
+    )
   }
 };
